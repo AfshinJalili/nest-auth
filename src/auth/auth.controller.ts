0b1750360f994eee962de21f50b5ee62d110ac06
Afshin Jalili <a.jalili@kaman.ir>
2023-07-23 14:20:45 +0330

    feat(auth): make auth guard global

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,17 +11,18 @@ import {
 import { AuthService } from './auth.service';
 import { User } from '../users/users.service';
 import { AuthGuard } from './auth.guard';
+import { Public } from '../decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.sigIn(signInDto.username, signInDto.password);
   }
-  @UseGuards(AuthGuard)
   @Get('profile')
   getProfile(@Request() req): User {
     return req.user;
